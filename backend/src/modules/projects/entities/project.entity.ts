52af4c53d32dc039a5704836fc4cf1a8e8852550
Alex-Farias <alexfabreu02@gmail.com>
2025-12-28 06:00:53 -0300

    implementing status column and enum in project entity

--- backend/src/modules/projects/entities/project.entity.ts
+++ backend/src/modules/projects/entities/project.entity.ts
@@ -1,26 +1,38 @@
 import { UserEntity } from "src/modules/users/entities/user.entity";
 import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 
+export enum ProjectStatus {
+    PENDING = 'pending',
+    IN_PROGRESS = 'in_progress',
+    COMPLETED = 'completed',
+}
+
 @Entity('project')
 export class ProjectEntity{
     @PrimaryGeneratedColumn('increment', {name: 'id_project'})
     id_project: number;
+    
     @Column({type: 'varchar', length: 255, nullable: false})
     name: string;
+    
     @Column({type: 'varchar', length: 255, nullable: false})
     description: string;
-    @Column({type: 'varchar', length: 255, nullable: false})
-    status: string; //TODO - transformar em model
+    
+    @Column({type: 'enum', enum: ProjectStatus, default: ProjectStatus.PENDING})
+    status: ProjectStatus;
+    
     @Column({type: 'varchar', length: 255, nullable: false})
     goals: string;
+    
     @Column({type: 'timestamp', default: () => 'CURRENT_TIMESTAMP'})
     createdAt: Date;
+    
     @Column({type: 'timestamp', default: () => 'CURRENT_TIMESTAMP', onUpdate: 'CURRENT_TIMESTAMP'})
     updatedAt: Date;
+    
     @Column({type: 'boolean', default: true})
     isActive: boolean;
-    @Column({ name: 'id_user', type: 'int', nullable: false })
-    userId: number;
+    
     @ManyToOne(() => UserEntity, { nullable: false })
     @JoinColumn({name: 'id_user'})
     user: UserEntity;
