40d8b3caf0506dfd9f90e10fe3f8e0a47921c1a0
Alex-Farias <alexfabreu02@gmail.com>
2025-12-28 06:01:16 -0300

    removing unused imports in register auth dto

--- backend/src/modules/auths/dto/register-auth.dto.ts
+++ backend/src/modules/auths/dto/register-auth.dto.ts
@@ -1,4 +1,3 @@
-import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
 import { UserCreateDto } from 'src/modules/users/dto/create-user.dto';
 
 export class AuthRegisterDto extends UserCreateDto {}
