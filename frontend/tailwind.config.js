213f6df74885dba307b53dd0e6fa9c15c085f9eb
Alex-Farias <alexfabreu02@gmail.com>
2025-12-28 05:58:48 -0300

    updating project dependencies and configuration files

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class',
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: 'rgb(var(--color-primary) / <alpha-value>)',
+        secondary: 'rgb(var(--color-secondary) / <alpha-value>)',
+        aux: 'rgb(var(--color-aux) / <alpha-value>)',
+        main: 'rgb(var(--color-text-main) / <alpha-value>)',
+        muted: 'rgb(var(--color-text-muted) / <alpha-value>)',
+        positive: 'rgb(var(--color-positive) / <alpha-value>)',
+        negative: 'rgb(var(--color-negative) / <alpha-value>)',
+      }
+    },
   },
   plugins: [],
 }
